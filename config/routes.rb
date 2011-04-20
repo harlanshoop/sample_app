be649a36f611f45a7e1eb393c9fcbe3560597f84
Harlan Shoop <harlan@jhsdatasystems.com>
2011-04-19 21:03:04 -0600

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
     match '/contact',   :to => 'pages#contact'
     match '/about',     :to => 'pages#about'
     match '/help',      :to => 'pages#help'
+    match '/home',      :to => 'pages#help'
 
     root :to => 'pages#home'
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -7,18 +7,18 @@ describe "LayoutLinks" do
         response.should have_selector('title', :content => "Home")
     end
 
-    it "should have a Contact page at '/'" do
-        get '/'
+    it "should have a Contact page at '/contact'" do
+        get '/contact'
         response.should have_selector('title', :content => "Contact")
     end
 
-    it "should have a About page at '/'" do
-        get '/'
+    it "should have a About page at '/about'" do
+        get '/about'
         response.should have_selector('title', :content => "About")
     end
 
-    it "should have a Help page at '/'" do
-        get '/'
+    it "should have a Help page at '/help'" do
+        get '/help'
         response.should have_selector('title', :content => "Help")
     end
 
